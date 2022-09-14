e63b07f04cfc4e707142f455616cbe71056ad162
alien2021brain <92595640+alien2021brain@users.noreply.github.com>
2022-09-15 00:19:54 +0530

    udpadeted root file

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import {createServer} from "http"
+import { Server } from 'socket.io';
+
+
+
 
 import Connection from './database/db.js';
 import Routes from './routes/Routes.js';
@@ -12,15 +17,74 @@ const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', Routes);
+
+app.use(cors());
+
+
+// listning on same server socket io
+const httpserver=createServer(app);
+const io=new Server(httpserver,{
+    cors: {
+        origin: "https://whats-app-web.onrender.com",
+        credentials: true
+      }
+
+
+
+});
+
+
+
+
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+// app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
 
-app.use(bodyParser.json({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
-app.use('/', Routes);
 
+
+
+let users = [];
+
+const addUser = (userData, socketId) => {
+    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
+}
+
+const removeUser = (socketId) => {
+    users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+    return users.find(user => user.sub === userId);
+}
+
+io.on('connection',(socket) => {
+    console.log('user connected')
+
+    //connect
+    socket.on("addUser", userData => {
+        addUser(userData, socket.id);
+        io.emit("getUsers", users);
+    })
+
+    //send message
+    socket.on('sendMessage', (data) => {
+        const user = getUser(data.receiverId);
+        io.to(user.socketId).emit('getMessage', data)
+    })
+
+    //disconnect
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        removeUser(socket.id);
+        io.emit('getUsers', users);
+    })
+})
+httpserver.listen(PORT,()=>{
+    console.log(`server is working on port${PORT}`)
+})
